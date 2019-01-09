2745a5ef4ff27cee85a6ee7e27537a62cb61badf
Ian Homer <ian.homer@gmail.com>
2019-01-09 18:54:19 +0000

    Change 2019-01-09 18:54:19

--- make-conflicts.sh
+++ make-conflicts.sh
@@ -6,6 +6,7 @@ branch=feature/conflicting
 if [[ `git branch -a | grep -c ${branch}` -eq 0 ]] ; then
   echo "Creating branch ${branch}"
   git checkout -b feature/conflicting
+  git git push --set-upstream origin ${branch}
 else
   echo "Switching to branch ${branch}"
   git checkout feature/conflicting
