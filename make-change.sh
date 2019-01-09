2674e3a5f6b0ced4c88822f86d9cba5084144b60
Ian Homer <ian.homer@gmail.com>
2019-01-09 22:17:32 +0000

    Make change on master

--- make-change.sh
+++ make-change.sh
@@ -5,13 +5,19 @@ HEADING="Make Change"
 . ./common.sh
 
 file=files/change.txt
+masterFile=files/master-change.txt
 branch=experiment/change
 DOCUMENTATION=documentation/make-change.md
 heading "${HEADING}"
 
 checkout ${branch}
 
+# Branch changes
 makeChange ${file}
 makeChange ${file}
 makeChange ${file}
+
+# Non-conflicting change
+git checkout master
+makeChange ${masterFile}
 
