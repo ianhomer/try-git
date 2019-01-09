112f6205e434e5e443ea22df5196675528249b78
Ian Homer <ian.homer@gmail.com>
2019-01-09 18:52:44 +0000

    Change 2019-01-09 18:52:44

---
+++ make-conflicts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+file=files/conflictable.txt
+branch=feature/conflicting
+
+if [[ `git branch -a | grep -c ${branch}` -eq 0 ]] ; then
+  echo "Creating branch ${branch}"
+  git checkout -b feature/conflicting
+else
+  echo "Switching to branch ${branch}"
+  git checkout feature/conflicting
+fi
+
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+echo "Change ${DATE}" >> ${file}
+git commit -am "Change ${DATE}"
